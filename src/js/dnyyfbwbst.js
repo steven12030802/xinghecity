626f7b9b7cfc9155b6551e57ecb34e8b25886b93
dnyyfb <dnyyfb@qq.com>
2024-01-23 23:11:10 +0800

    新年更新

--- src/js/dnyyfbwbst.js
+++ src/js/dnyyfbwbst.js
@@ -1,4 +1,4 @@
-console.log("Copyright 2018-2023 星河城工作室 All Right Reserved. ")
+console.log("Copyright 2018-2024 星河城工作室 All Right Reserved. ")
 console.warn("Without permission, it is forbidden to carry any code of this website!")
 console.warn("这不仅是一个警告，也是一个彩蛋")
 console.warn("未经允许，禁止复制这个网站的任何源码！")
